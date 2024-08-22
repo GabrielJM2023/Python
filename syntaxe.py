3bc7b823e87c9f9edee19af0fcfb728f71816fcb
GabrielJM2023 <jardimgabriel2022@gmail.com>
2024-08-22 06:57:47 -0300

    Introdução
    Alguns conceitos da linguagem

---
+++ syntaxe.py
@@ -0,0 +1,21 @@
+# - comentario de uma linha 
+
+"""
+Comentario de varias linhas 
+"""
+
+# Declaração de variaveis padrão de linguagens, sem caracter especial no inicio da variavel, python é caseSensitive  e tem tipagem dinamica
+nome = "Gabriel" +" "+ \
+"Jardim"
+
+nome = 1
+# "\" é uma quebra de linha, se tirar da erro
+
+# Python não tem um final especifico, ele utiliza a identação 
+
+if nome == "Gabriel":
+    print(nome)
+else:
+    if nome == "Gabriel Jardim":
+        print(nome)
+
